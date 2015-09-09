d178e9d6b59ed341d2c1eb4a685cca07228c4149
BoJiang <188166614@qq.com>
2015-09-09 19:19:47 +0800

    Initial Commit

---
+++ fbxLoader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fbxLoader
+//
+//  Created by vk on 15/9/9.
+//  Copyright (c) 2015年 quxiu8. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
